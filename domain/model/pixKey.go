d0109d1e87639f798cc82ab8309cd7815313e009
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-12-12 20:56:20 -0300

    :construction: defines the interface

--- domain/model/pixKey.go
+++ domain/model/pixKey.go
@@ -7,6 +7,14 @@ import (
 	"time"
 )
 
+type PixKeyRepositoryInterface interface {
+	RegisterKey(pixKey *PixKey) (*PixKey, error)
+	FindKeyByKind(key string, kind string) (*PixKey, error)
+	AddBank(bank *Bank) error
+	AddAccount(account Account) error
+	FindAccount(id string) (*Account, error)
+}
+
 type PixKey struct {
 	Base      `valid:"required" valid:"notnull"`
 	Kind      string   `json:"kind" valid:"notnull"`
